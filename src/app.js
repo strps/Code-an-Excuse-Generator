883fd22c1ea3654ec1aa950e3ad0813eec8b4a6c
strps <csrstrps@gmail.com>
2022-11-19 19:33:10 +0000

    Adding Code Generator functionality website

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  var excuseText = "";
+  excuseText += who[Math.floor(Math.random() * 4)] + " ";
+  excuseText += action[Math.floor(Math.random() * 4)] + " ";
+  excuseText += what[Math.floor(Math.random() * 3)] + " ";
+  excuseText += when[Math.floor(Math.random() * 5)];
+
+  var excuse = document.getElementById("excuse");
+  excuse.innerHTML = excuseText;
 };
